9470bb86a8c51f9036acf675dc531f8d9f69018a
Colin Osborn <colin.osborn@gmail.com>
2016-02-10 09:00:04 -0700

    started initials

--- codebreakers/initials.rb
+++ codebreakers/initials.rb
@@ -1,18 +1,49 @@
-array = ["Taylor", "Swift"]
-puts array.map {|x| x[0]}.inject(:+)
+class Initials
+  attr_reader :message
+
+  def initialize(message)
+    @message = message
+  end
+
+  def get_initials
+    
+
+  end
+end
+
+if __FILE__ == $0
+i = Initials.new(ARGV[0..-1])
+
+end
 
 
 
-# Problem 2
-array1 = [*"a".."m"]
-# puts array1
-array2 = [*"n".."z"]
-puts array2
-array1.zip(array2)
-array1
-# chars - splits it so each letter is it's own string
-string = "emily"
-# string.chars.map {|x| x.downcase == complete.fetch(x)}
-sentence = "trivial"
-sentence.map {|x| x.chars}.map {|y| complete.fetch(y)}
-spaced.flatten.map {|x| complete[x]}
+
+
+
+
+#
+# array = ["Taylor Swift"]
+# puts array.map {|x| x[0]}.inject(:+)
+#
+#
+#
+#
+#
+#
+#
+#
+#
+# # Problem 2
+# array1 = [*"a".."m"]
+# # puts array1
+# array2 = [*"n".."z"]
+# puts array2
+# array1.zip(array2)
+# array1
+# # chars - splits it so each letter is it's own string
+# string = "emily"
+# # string.chars.map {|x| x.downcase == complete.fetch(x)}
+# sentence = "trivial"
+# sentence.map {|x| x.chars}.map {|y| complete.fetch(y)}
+# spaced.flatten.map {|x| complete[x]}
