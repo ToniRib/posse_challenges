6e1f5c6c2b7a19f569d7d94fd4be2e1ee4263083
ToniRib <tonimarierib@gmail.com>
2016-02-13 16:43:03 -0700

    Remove toni folder

---
+++ codebreakers/caesar.js
@@ -0,0 +1,10 @@
+var cipher = "";
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i",
+                 "j", "k", "l", "m", "n", "o", "p", "q", "r",
+                 "s", "t", "u", "v", "w", "x", "y", "z"];
+var key = process.argv[2];
+var capitalFlag = false;
+
+console.log(process.argv[3]);
+
+console.log(cipher);
