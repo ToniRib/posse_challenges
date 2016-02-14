42c0aa8f5cb87b848f1fa64a739df96e263f3fc9
ToniRib <tonimarierib@gmail.com>
2016-02-13 18:19:27 -0700

    initial working solution for vigenere

--- codebreakers/vigenere.rb
+++ codebreakers/vigenere.rb
@@ -4,33 +4,47 @@ class Vigenere
   attr_reader :key, :message
 
   def initialize(key, message)
-    @key = key.chars.map { |l| letter_map[l] }
+    @key = key.chars.map { |l| letter_map[l.downcase] }
     @message = message
   end
 
   def letter_map
-    {
-      "a" => 1,  "b" => 2,  "c" => 3,  "d" => 4,  "e" => 5,  "f" => 6,
-      "g" => 7,  "h" => 8,  "i" => 9,  "j" => 10, "k" => 11, "l" => 12,
-      "m" => 13, "n" => 14, "o" => 15, "p" => 16, "q" => 17, "r" => 18,
-      "s" => 19, "t" => 20, "u" => 21, "v" => 22, "w" => 23, "x" => 24,
-      "y" => 25, "z" => 26
-    }
+    [*"a".."z"].zip([*1..26]).to_h
   end
 
   def encrypt_message
-    encrypted = message.chars.map.with_index do |l, idx|
+    encrypted = ""
+    i = -1
+
+    message.chars.each do |l|
       if letter_map.include?(l)
-        binding.pry
-        letter_map.key((letter_map[l] + key[idx % key.length]) % 27)
+        i = i + 1
+        num = convert_letter_to_number(l.downcase)
+        this_key = get_key_number(i)
+        new_index = (num + this_key) % 26
+        new_index = 26 if new_index == 0
+        encrypted << letter_map.key(new_index)
       elsif letter_map.include?(l.downcase)
-        letter_map.key((letter_map[l.downcase] + key[idx % key.length]) % 27).upcase
+        i = i + 1
+        num = convert_letter_to_number(l.downcase)
+        this_key = get_key_number(i)
+        new_index = (num + this_key) % 26
+        new_index = 26 if new_index == 0
+        encrypted << letter_map.key(new_index).upcase
       else
-        l
+        encrypted << l
       end
     end
 
-    encrypted.join
+    encrypted
+  end
+
+  def convert_letter_to_number(letter)
+    letter_map[letter]
+  end
+
+  def get_key_number(i)
+    key[i % key.length]
   end
 end
 
